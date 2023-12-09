ddc57b42507cb848e8e12b4ea7ed0c3b33640eda
ShashankShekhar9839 <111214162+ShashankShekhar9839@users.noreply.github.com>
2023-12-09 21:45:15 +0530

    example done for class component

--- useState/useStateHook/src/App.jsx
+++ useState/useStateHook/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 
 import './App.css'
+import ClassState from './components/ClassComponentState/ClassState'
+import ParentClassState from './components/ClassComponentState/ParentClassState'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+      {/* <ClassState/> */} 
+      <ParentClassState/>
     </>
   )
 }
---
+++ useState/useStateHook/src/components/ClassComponentState/ParentClassState.jsx
@@ -0,0 +1,45 @@
+import { Component } from "react";
+import ClassState from "./ClassState";
+
+class ParentClassState extends Component {
+     
+        constructor(props) {
+        super(props);
+        this.state = {
+            count : 0,
+            color : 'black',
+        }
+    } 
+    
+     increaseCount = () => {
+        this.setState(prevState=> {
+            return {count : prevState.count + 1, 
+            color : 'blue',
+            }
+        })
+    } 
+
+    decreaseCount = () => {
+        this.setState(prevState=> { 
+            if(prevState.count > 0) {
+                return {count : prevState.count - 1, 
+                    color : 'red',
+                    }
+            }
+            else {
+                alert("you can't decrease me below 0" );
+            }
+
+        })
+    }
+
+    render(){
+        return (
+            <div className="parent-class-wrapper">
+             <ClassState count = {this.state.count} color = {this.state.color} handleDecrease = {this.decreaseCount} handleIncrease = {this.increaseCount} />
+            </div>
+        )
+    }
+}
+
+export default ParentClassState;
